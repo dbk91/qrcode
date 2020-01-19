8c9ce9a5ff9290618b3711efc8cdc11746733552
Daniel Kelly <daniel.brandon.kelly@gmail.com>
2020-01-18 22:26:32 -0500

    Add repository link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import App from 'next/app'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Box from '@material-ui/core/Box'
+import IconButton from '@material-ui/core/IconButton'
+import Link from '@material-ui/core/Link'
 import { ThemeProvider } from '@material-ui/core/styles'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import GithubIcon from '@material-ui/icons/Github'
 
 enum ThemeType {
   LIGHT = 'light',
@@ -44,9 +51,34 @@ const createTheme = (type: ThemeType) =>
 
 function MyApp({ Component, pageProps }) {
   const theme = React.useMemo(() => createTheme(ThemeType.LIGHT), [])
+  const [padding, setPadding] = React.useState(0)
+  const matches = useMediaQuery(theme.breakpoints.up('sm'))
+  const measureAppBar = React.useCallback(
+    node => {
+      if (node) {
+        setPadding(node.clientHeight + 8)
+      }
+    },
+    [matches],
+  )
 
   return (
     <ThemeProvider theme={theme}>
+      <AppBar innerRef={measureAppBar} elevation={0}>
+        <Toolbar>
+          <Box display="flex" flexGrow={1} />
+          <Link
+            href="https://github.com/dbk91/qrcode"
+            target="_blank"
+            component={IconButton}
+            edge="end"
+            color="inherit"
+          >
+            <GithubIcon />
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Box m={`${padding}px`} />
       <Component {...pageProps} />
     </ThemeProvider>
   )
