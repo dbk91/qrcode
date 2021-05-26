f4a4c2e702e927cf2d7a63728beaf229c2facacc
Daniel Kelly <daniel.brandon.kelly@gmail.com>
2021-05-25 20:41:30 -0400

    Split error page into separate components

---
+++ pages/500.tsx
@@ -0,0 +1,18 @@
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+
+function Custom404() {
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h3">
+        <Box textAlign="center">500</Box>
+        <Box textAlign="center" color="text.secondary">
+          Internal Server Error
+        </Box>
+      </Typography>
+    </Container>
+  )
+}
+
+export default Custom404
