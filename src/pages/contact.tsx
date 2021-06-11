98f3cf09db9401da3c449a496e12c8f3ba23fd3b
Daniel Kelly <daniel.brandon.kelly@gmail.com>
2021-06-11 11:53:45 -0400

    Add titles to each page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Head from 'next/head'
 
 import PageLayout from 'components/PageLayout'
 import ContactForm from 'components/ContactForm'
 
 function ContactPage() {
-  return <PageLayout render={({ setQrCodeText }) => <ContactForm onSuccess={setQrCodeText} />} />
+  return (
+    <>
+      <Head>
+        <title>QR Code Generator - Contact</title>
+      </Head>
+      <PageLayout render={({ setQrCodeText }) => <ContactForm onSuccess={setQrCodeText} />} />
+    </>
+  )
 }
 
 export default ContactPage
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Head from 'next/head'
 
 import PageLayout from 'components/PageLayout'
 import WifiForm from 'components/WifiForm'
 
 const HomePage = () => {
-  return <PageLayout render={({ setQrCodeText }) => <WifiForm onSuccess={setQrCodeText} />} />
+  return (
+    <>
+      <Head>
+        <title>QR Code Generator - Wi-Fi</title>
+      </Head>
+      <PageLayout render={({ setQrCodeText }) => <WifiForm onSuccess={setQrCodeText} />} />
+    </>
+  )
 }
 
 export default HomePage
