6a59d04f4454cca6a273bc372890c0243a55d005
Daniel Kelly <me@dankelly.dev>
2022-06-24 17:01:55 -0500

    Switches from yarn to pnpm

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,4 @@
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import createMuiTheme from '@material-ui/core/styles/createTheme'
 
 export enum ThemeType {
   LIGHT = 'light',
